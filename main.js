cfcf494459d3ad41addb1da5bc881bbbc593a581
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-03-28 14:12:56 +0200

    movement

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-perso = document.getElementById("perso");
+var perso = document.getElementById("perso");
+var info = document.getElementById("Info")
 
-perso.addEventListener("keyDown",movement);
-
-function movement(event) {
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode == 37) {
+        perso.style.left = parseInt(perso.style.left) - 10 + "px";
+    }
+    if (event.keyCode == 39) {
+        perso.style.left = parseInt(perso.style.left) + 10 + "px";
+    }
     
-    
+});
+function jump(){
+while (perso.style.top < 1000) {
+    perso.style.top = parseInt(perso.style.top) + 10 + "px";
 }
-
-
+while (perso.style.top > 0) {
+    perso.style.top = parseInt(perso.style.top) - 10 + "px";
+}}
+info.innerHTML = "Score"
+jump();

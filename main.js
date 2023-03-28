0451ba921776ef5f67379895fafcf7ab5648a26b
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2023-03-28 16:07:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -24,27 +24,31 @@ function jump() {
                     } else {
                         perso.style.top = parseInt(perso.style.top) + 10 + "px";
                         jumpCount--;
+                        checkBlock();
                     }
                 }, 20);
             } else {
                 perso.style.top = parseInt(perso.style.top) - 10 + "px";
                 jumpCount++;
-                checkBlock();
             }
         }, 20);
     }
 }
 
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode == 32) {
+        jump();
+    }
+});
+
 info.innerHTML = "Score";
 
 // function to check if player is on a block
+var blocks = document.getElementsByClassName("block");
 function checkBlock() {
-    var blocks = document.getElementsByClassName("block");
     for (let i = 0; i < blocks.length; i++) {
         if (parseInt(perso.style.left) >= parseInt(blocks[i].style.left) && parseInt(perso.style.left) <= parseInt(blocks[i].style.left) + 50 && parseInt(perso.style.top) >= parseInt(blocks[i].style.top) && parseInt(perso.style.top) <= parseInt(blocks[i].style.top) + 70) {
             jump();
         }
     }
 }
-
-jump();
